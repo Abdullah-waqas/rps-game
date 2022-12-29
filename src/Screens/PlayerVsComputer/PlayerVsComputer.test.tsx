7492a38fb0644fb0b5fdc390e8ac2bec4efdbae4
Abdullah Waqas <dev.waqas08@gmail.com>
2022-12-29 20:16:48 +0100

    remove code duplication

--- src/Screens/PlayerVsComputer/PlayerVsComputer.test.tsx
+++ src/Screens/PlayerVsComputer/PlayerVsComputer.test.tsx
@@ -17,93 +17,93 @@ describe('<ComputerVsComputer />', () => {
     expect(setModeSpy).toHaveBeenCalled();
   });
 
-  // it('should call getRandomOption func', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: PAPER, value2: ROCK });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   expect(getRandomOptionMock).not.toHaveBeenCalled();
-  //   const paperBtn = screen.getByTestId('test-paper_btn');
-  //   userEvent.click(paperBtn);
-  //   expect(getRandomOptionMock).toHaveBeenCalled();
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should call getRandomOption func', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: PAPER, value2: ROCK });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    expect(getRandomOptionMock).not.toHaveBeenCalled();
+    const paperBtn = screen.getByTestId('test-paper_btn');
+    userEvent.click(paperBtn);
+    expect(getRandomOptionMock).toHaveBeenCalled();
+    getRandomOptionMock.mockRestore();
+  });
 
-  // it('should tie the game', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const rockBtn = screen.getByTestId('test-rock_btn');
-  //   userEvent.click(rockBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Tie');
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should tie the game', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const rockBtn = screen.getByTestId('test-rock_btn');
+    userEvent.click(rockBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Tie');
+    getRandomOptionMock.mockRestore();
+  });
 
-  // it('should tie the game', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const rockBtn = screen.getByTestId('test-rock_btn');
-  //   userEvent.click(rockBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Tie');
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should tie the game', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const rockBtn = screen.getByTestId('test-rock_btn');
+    userEvent.click(rockBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Tie');
+    getRandomOptionMock.mockRestore();
+  });
 
-  // it('should win Human', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const paperBtn = screen.getByTestId('test-paper_btn');
-  //   userEvent.click(paperBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Human win');
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should win Human', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const paperBtn = screen.getByTestId('test-paper_btn');
+    userEvent.click(paperBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Human win');
+    getRandomOptionMock.mockRestore();
+  });
 
-  // it('should win Computer', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const paperBtn = screen.getByTestId('test-scissor_btn');
-  //   userEvent.click(paperBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Computer win');
-  //   getRandomOptionMock.mockRestore();
-  // });
-  // it('should win Human', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const paperBtn = screen.getByTestId('test-paper_btn');
-  //   userEvent.click(paperBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Human win');
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should win Computer', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const paperBtn = screen.getByTestId('test-scissor_btn');
+    userEvent.click(paperBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Computer win');
+    getRandomOptionMock.mockRestore();
+  });
+  it('should win Human', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const paperBtn = screen.getByTestId('test-paper_btn');
+    userEvent.click(paperBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Human win');
+    getRandomOptionMock.mockRestore();
+  });
 
-  // it('should win Computer', () => {
-  //   const setModeSpy = jest.fn();
-  //   const getRandomOptionMock = jest
-  //     .spyOn(method, 'getRandomOption')
-  //     .mockReturnValue({ value1: ROCK, value2: PAPER });
-  //   render(<PlayerVsComputer setMode={setModeSpy} />);
-  //   const paperBtn = screen.getByTestId('test-scissor_btn');
-  //   userEvent.click(paperBtn);
-  //   const gameResult = screen.getByTestId('game-result');
-  //   expect(gameResult.textContent).toEqual('Result: Computer win');
-  //   getRandomOptionMock.mockRestore();
-  // });
+  it('should win Computer', () => {
+    const setModeSpy = jest.fn();
+    const getRandomOptionMock = jest
+      .spyOn(method, 'getRandomOption')
+      .mockReturnValue({ value1: ROCK, value2: PAPER });
+    render(<PlayerVsComputer setMode={setModeSpy} />);
+    const paperBtn = screen.getByTestId('test-scissor_btn');
+    userEvent.click(paperBtn);
+    const gameResult = screen.getByTestId('game-result');
+    expect(gameResult.textContent).toEqual('Result: Computer win');
+    getRandomOptionMock.mockRestore();
+  });
 });
