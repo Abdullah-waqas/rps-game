4e1878a438645ce397b468f9aab655f66c00b3dc
Abdullah Waqas <dev.waqas08@gmail.com>
2022-12-29 19:52:38 +0100

    test if duplication works in codeclimate

--- src/Screens/ComputerVsComputer/ComputerVsComputer.test.tsx
+++ src/Screens/ComputerVsComputer/ComputerVsComputer.test.tsx
@@ -17,66 +17,66 @@ describe('<ComputerVsComputer />', () => {
     expect(setModeSpy).toHaveBeenCalled();
   });
 
-  it('should call getRandomOption func', () => {
-    jest.useFakeTimers();
-    const setModeSpy = jest.fn();
-    const getRandomOptionMock = jest
-      .spyOn(method, 'getRandomOption')
-      .mockReturnValue({ value1: PAPER, value2: ROCK });
-    render(<ComputerVsComputer setMode={setModeSpy} />);
-    act(() => {
-      jest.runAllTimers();
-    });
-    expect(getRandomOptionMock).toHaveBeenCalled();
-    jest.useRealTimers();
-    getRandomOptionMock.mockRestore();
-  });
+  // it('should call getRandomOption func', () => {
+  //   jest.useFakeTimers();
+  //   const setModeSpy = jest.fn();
+  //   const getRandomOptionMock = jest
+  //     .spyOn(method, 'getRandomOption')
+  //     .mockReturnValue({ value1: PAPER, value2: ROCK });
+  //   render(<ComputerVsComputer setMode={setModeSpy} />);
+  //   act(() => {
+  //     jest.runAllTimers();
+  //   });
+  //   expect(getRandomOptionMock).toHaveBeenCalled();
+  //   jest.useRealTimers();
+  //   getRandomOptionMock.mockRestore();
+  // });
 
-  it('should tie the game', () => {
-    jest.useFakeTimers();
-    const setModeSpy = jest.fn();
-    const getRandomOptionMock = jest
-      .spyOn(method, 'getRandomOption')
-      .mockReturnValue({ value1: PAPER, value2: PAPER });
-    render(<ComputerVsComputer setMode={setModeSpy} />);
-    act(() => {
-      jest.runAllTimers();
-    });
-    const gameResult = screen.getByTestId('game-result');
-    expect(gameResult.textContent).toEqual('Tie');
-    jest.useRealTimers();
-    getRandomOptionMock.mockRestore();
-  });
+  // it('should tie the game', () => {
+  //   jest.useFakeTimers();
+  //   const setModeSpy = jest.fn();
+  //   const getRandomOptionMock = jest
+  //     .spyOn(method, 'getRandomOption')
+  //     .mockReturnValue({ value1: PAPER, value2: PAPER });
+  //   render(<ComputerVsComputer setMode={setModeSpy} />);
+  //   act(() => {
+  //     jest.runAllTimers();
+  //   });
+  //   const gameResult = screen.getByTestId('game-result');
+  //   expect(gameResult.textContent).toEqual('Tie');
+  //   jest.useRealTimers();
+  //   getRandomOptionMock.mockRestore();
+  // });
 
-  it('should win computer 1', () => {
-    jest.useFakeTimers();
-    const setModeSpy = jest.fn();
-    const getRandomOptionMock = jest
-      .spyOn(method, 'getRandomOption')
-      .mockReturnValue({ value1: PAPER, value2: ROCK });
-    render(<ComputerVsComputer setMode={setModeSpy} />);
-    act(() => {
-      jest.runAllTimers();
-    });
-    const gameResult = screen.getByTestId('game-result');
-    expect(gameResult.textContent).toEqual('Computer 1 win');
-    jest.useRealTimers();
-    getRandomOptionMock.mockRestore();
-  });
+  // it('should win computer 1', () => {
+  //   jest.useFakeTimers();
+  //   const setModeSpy = jest.fn();
+  //   const getRandomOptionMock = jest
+  //     .spyOn(method, 'getRandomOption')
+  //     .mockReturnValue({ value1: PAPER, value2: ROCK });
+  //   render(<ComputerVsComputer setMode={setModeSpy} />);
+  //   act(() => {
+  //     jest.runAllTimers();
+  //   });
+  //   const gameResult = screen.getByTestId('game-result');
+  //   expect(gameResult.textContent).toEqual('Computer 1 win');
+  //   jest.useRealTimers();
+  //   getRandomOptionMock.mockRestore();
+  // });
 
-  it('should win computer 2', () => {
-    jest.useFakeTimers();
-    const setModeSpy = jest.fn();
-    const getRandomOptionMock = jest
-      .spyOn(method, 'getRandomOption')
-      .mockReturnValue({ value1: PAPER, value2: SCISSOR });
-    render(<ComputerVsComputer setMode={setModeSpy} />);
-    act(() => {
-      jest.runAllTimers();
-    });
-    const gameResult = screen.getByTestId('game-result');
-    expect(gameResult.textContent).toEqual('Computer 2 win');
-    jest.useRealTimers();
-    getRandomOptionMock.mockRestore();
-  });
+  // it('should win computer 2', () => {
+  //   jest.useFakeTimers();
+  //   const setModeSpy = jest.fn();
+  //   const getRandomOptionMock = jest
+  //     .spyOn(method, 'getRandomOption')
+  //     .mockReturnValue({ value1: PAPER, value2: SCISSOR });
+  //   render(<ComputerVsComputer setMode={setModeSpy} />);
+  //   act(() => {
+  //     jest.runAllTimers();
+  //   });
+  //   const gameResult = screen.getByTestId('game-result');
+  //   expect(gameResult.textContent).toEqual('Computer 2 win');
+  //   jest.useRealTimers();
+  //   getRandomOptionMock.mockRestore();
+  // });
 });
